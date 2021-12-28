21232231e0e4f9ffb18b124abf26a5c4f58c8f04
backchristoffer <backchristoffer@hotmail.com>
2021-12-28 16:12:52 +0100

    request change

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func main(){
-	fmt.Println("Hello world")
+func main() {
+	request, error := http.Get("http://date.jsonest.com/ll")
+	if error != nil {
+		log.Fatal("Was not able to get request", error)
+	}
+	defer request.Body.Close()
+	response, error := io.ReadAll(request.Body)
+
+	fmt.Printf(string(response))
 }
